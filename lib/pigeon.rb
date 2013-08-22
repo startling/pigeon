1abf154803346982139e433dae56a34b1eb74122
startling <tdixon51793@gmail.com>
2013-08-22 04:02:28 -0400

    Made pigeon.rb an actual module.

--- lib/pigeon.rb
+++ lib/pigeon.rb
@@ -7,7 +7,6 @@ require 'nokogiri'
 require 'kramdown'
 require 'chronic'
 require 'haml'
-require 'trollop'
 
 class Pigeon
   attr :actions
@@ -47,166 +46,121 @@ class Pigeon
   alias tsort_each_child dependencies
 end
 
-markdown = {
-  :requires => [:source],
-  :provide  => :html,
-  :block    => lambda do |source|
-    md = Kramdown::Document.new (File.read source),
-      :coderay_line_numbers => nil,
-      :coderay_css => :class
-    file = Tempfile.new source
-    file.write md.to_html
-    file.rewind
-    return file
+module Pigeon::Action
+  def self.markdown
+    {
+      :requires => [:source],
+      :provide  => :html,
+      :block    => lambda do |source|
+        md = Kramdown::Document.new (File.read source),
+          :coderay_line_numbers => nil,
+          :coderay_css => :class
+        file = Tempfile.new source
+        file.write md.to_html
+        file.rewind
+        return file
+      end
+    }
   end
-}
 
-# Parse HTML with nokogiri
-parseHtml = {
-  :requires => [:html],
-  :provide  => :document,
-  :block    => lambda do |html|
-    document = Nokogiri::HTML html, nil, "utf-8"
-    html.rewind
-    return document
+  # Parse HTML with nokogiri
+  def self.parseHtml
+    {
+      :requires => [:html],
+      :provide  => :document,
+      :block    => lambda do |html|
+        document = Nokogiri::HTML html, nil, "utf-8"
+        html.rewind
+        return document
+      end
+    }
   end
-}
 
-# Stick the title in.
-getTitle = {
-  :requires => [:document],
-  :provide  => :title,
-  :block    => lambda do |document|
-    h1s = document.css('h1')
-    if h1s.length > 0
-      return h1s[0].text
-    else
-      return nil
-    end
+  # Stick the title in.
+  def self.getTitle
+    {
+      :requires => [:document],
+      :provide  => :title,
+      :block    => lambda do |document|
+        h1s = document.css('h1')
+        if h1s.length > 0
+          return h1s[0].text
+        else
+          return nil
+        end
+      end
+    }
   end
-}
-
-# Stick the date in.
-getDate = {
-  :requires => [:document],
-  :provide  => :date,
-  :block    => lambda do |document|
-    times = document.css 'time[pubdate]'
-    if times.length > 0
-      return Chronic.parse times[0]["datetime"]
-    else
-      return nil
-    end
+  
+  # Stick the date in.
+  def self.getDate
+    {
+      :requires => [:document],
+      :provide  => :date,
+      :block    => lambda do |document|
+        times = document.css 'time[pubdate]'
+        if times.length > 0
+          return Chronic.parse times[0]["datetime"]
+        else
+          return nil
+        end
+      end
+    }
   end
-}
-
-# Render each thing with a template.
-template = {
-  :requires => [:title, :date, :html, :options],
-  :provide  => :output,
-  :block    => lambda do |title, date, html, options|
-    page = Haml::Engine.new <<-END.gsub(/^ {6}/, '')
-      !!! 5
-      %html
-        %head
-          %meta{ :charset => "utf-8" }
-          - if options.stylesheet
-            %link{ :rel   => "stylesheet",
-                   :type  => "text/css",
-                   :media => "screen",
-                   :href  => options.stylesheet }
-          %title
-            = options.title
-        %body
-          %article
-            ~ html.read
-      END
-    return page.render Object.new,
-      :title   => title,
-      :date    => date,
-      :html    => html,
-      :options => options
+  
+  # Render each thing with a template.
+  def self.template
+    {
+      :requires => [:title, :date, :html, :options],
+      :provide  => :output,
+      :block    => lambda do |title, date, html, options|
+        page = Haml::Engine.new <<-END.gsub(/^ {10}/, '')
+          !!! 5
+          %html
+            %head
+              %meta{ :charset => "utf-8" }
+              - if options.stylesheet
+                %link{ :rel   => "stylesheet",
+                       :type  => "text/css",
+                       :media => "screen",
+                       :href  => options.stylesheet }
+              %title
+                = options.title
+            %body
+              %article
+                ~ html.read
+          END
+        return page.render Object.new,
+          :title   => title,
+          :date    => date,
+          :html    => html,
+          :options => options
+      end
+    }
   end
-}
-
-# Deduce the filename for each article.
-filename = {
-  :requires => [:title, :date],
-  :provide  => :filename,
-  :block    => lambda do |title, date|
-    pretty = date.strftime "%Y-%m-%d"
-    return "#{pretty}-#{title}.html" 
+  
+  # Deduce the filename for each article.
+  def self.filename
+    {
+      :requires => [:title, :date],
+      :provide  => :filename,
+      :block    => lambda do |title, date|
+        pretty = date.strftime "%Y-%m-%d"
+        return "#{pretty}-#{title}.html" 
+      end
+    }
   end
-}
-
-# Create an action writing :output to :filename.
-writeOut = {
-  :requires => [:output, :filename, :options],
-  :provide  => nil,
-  :block    => lambda do |output, filename, options|
-    f = File.open File.join(options[:output], filename), "w"
-    f.write output
-    f.close
+  
+  # Create an action writing :output to :filename.
+  def self.writeOut
+    {
+      :requires => [:output, :filename, :options],
+      :provide  => nil,
+      :block    => lambda do |output, filename, options|
+        f = File.open File.join(options[:output], filename), "w"
+        f.write output
+        f.close
+      end
+    }
   end
-}
-
-# Blah
-options = Trollop::options do
-  opt :title, "blog title",
-    :type => :string, :default => "blog"
-  opt :stylesheet, "css URI",
-    :type => :string
-  opt :with, "include another file",
-    :type => :string, :multi => true
-  opt :input, "input directory",
-    :type => :string, :default => "."
-  opt :output, "output directory",
-    :type => :string, :required => true
 end
-
-main = Pigeon.new [
-  markdown, parseHtml,
-  getTitle, getDate,
-  template, filename,
-  writeOut
-]
-
-articles = Dir.glob(File.join options[:input], "*.markdown")
-  .map { |a| main.execute :source => a, :options => options }
-
-index = Haml::Engine.new <<-END.gsub(/^ {2}/, '')
-  !!! 5
-  %html
-    %head
-      %meta{ :charset => "utf-8" }
-      - if options.stylesheet
-        %link{ :rel   => "stylesheet",
-               :type  => "text/css",
-               :media => "screen",
-               :href  => options.stylesheet }
-      %title
-        = options.title
-    %body
-      %article
-        %h1 Blog Posts
-        %ul.articles
-          - articles.each do |ar|
-            %li
-              - if ar[:date]
-                %time{ :datetime => ar[:date] }
-                  = ar[:date].strftime("%d %b %Y")
-              - else
-                %time.unknown
-              - if ar[:title]
-                %a.article{ :href => ar[:filename] }
-                  = ar[:title]
-              - else
-                %a.article.empty{ :href => ar[:filename] }
-  END
-f = File.open(File.join(options[:output], "index.html"), "w")
-i = index.render Object.new,
-  :articles => articles.sort { |x, y| y[:date] <=> x[:date] },
-  :options  => options
-f.write i
-f.close
-
